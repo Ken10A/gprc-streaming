d8fd5213dc0052bd8221815abac5e704c97a156e
KENN <cent.rngttwt@gmail.com>
2021-07-24 17:43:25 +0900

    add: go server

---
+++ server_streaming/server/database.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	pb "practical_grpc/streaming/server/proto"
+)
+
+type DatabaseService struct{}
+
+func (db *DatabaseService) Search(r *pb.SearchRequest, s pb.Database_SearchServer) error {
+	responses := []string{
+		"Highest ranked content",
+		"Some ranked content",
+		"Some ranked content",
+		"Lowest ranked content",
+	}
+
+	for idx, resp := range responses {
+		result := &pb.SearchResponse{
+			MatchedTerm: r.Term,
+			Rank:        int32(idx + 1),
+			Content:     resp,
+		}
+
+		err := s.Send(result)
+		if err != nil {
+			log.Printf("Error sending message to the client: %v", err)
+			return err
+		}
+	}
+
+	return nil
+}
