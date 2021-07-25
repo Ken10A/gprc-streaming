2337077f55bc4391fff4129b34621448967a8859
KENN <cent.rngttwt@gmail.com>
2021-07-25 22:15:56 +0900

    fix: fix client typo

--- server_streaming/database/client.rb
+++ server_streaming/database/client.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal : true
 
 $LOAD_PATH.unshift('.')
-require 'proto.database_service_pb'
+require 'proto/database_services_pb'
 
-Stub    = Database::Stub
-Request = SearchRequest
+Stub    = Practical::Grpc::V1::Database::Stub
+Request = Practical::Grpc::V1::SearchRequest
 
-term ARGV.first
+term = ARGV.first
 
 begin
     stub    = Stub.new('localhost:8080', :this_channel_is_insecure)
-    request = Request.new(term: term, max_result: 10)
+    request = Request.new(term: term, max_results: 10)
 
-    stub.search(request).each do |reponse|
+    stub.search(request).each do |response|
         puts "Term : #{response.matched_term}"
         puts "Rank : #{response.rank}"
         puts "Content: #{response.content}"
         puts
     end
 
-rescure StarndardError => e
+rescue StandardError => e
     code = e.respond_to?(:code) ? e.code : 'Unknown'
     puts "Code: #{code}, Type: '#{e.class}', Message: #{e.message}"
 end
