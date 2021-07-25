9e5332835b61bb5051ac3ae90f93f524e614d849
KENN <cent.rngttwt@gmail.com>
2021-07-25 22:17:14 +0900

    add: create go main

---
+++ server_streaming/server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net"
+	pb "protobuf/proto"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	s := grpc.NewServer()
+	pb.RegisterDatabaseServer(s, new(DatabaseService))
+
+	log.Print("Starting RPC server on port 8080...")
+	list, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("failed to setup tcp listener: %v", err)
+	}
+
+	err = s.Serve(list)
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+
+}
