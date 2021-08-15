67562486c57d255b8d7a83d94887f25eb66fa82f
KENN <cent.rngttwt@gmail.com>
2021-08-15 19:21:17 +0900

    [client_streaming] add: go archiver server

---
+++ client_streaming/server/archiver.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"archive/zip"
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+
+	pb "client_streaming/proto"
+)
+
+type ArchiverService struct{}
+
+func (as *ArchiverService) Zip(stream pb.Archiver_ZipServer) error {
+	buf := new(bytes.Buffer)
+	zf := zip.NewWriter(buf)
+
+	for {
+		req, err := stream.Recv()
+		err = zf.Close()
+		if err == io.EOF {
+			if err != nil {
+				log.Print("Error creating the zip file: %v", err)
+				return err
+			}
+			return stream.SendAndClose(&pb.ZipResponse{
+				ZippedContents: buf.Bytes(),
+			})
+		}
+
+		if err != nil {
+			log.Print("Error reading the request: %v", err)
+			return err
+		}
+
+		f, err := zf.Create(req.FileName)
+		if err != nil {
+			log.Print("Error creating the zip file entry: %v", err)
+			return err
+		}
+
+		_, err = f.Write(req.Contents)
+		if err != nil {
+			fmt.Println("Error writing zip file: %v", err)
+			return err
+		}
+	}
+}
---
+++ client_streaming/server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	pb "client_streaming/proto"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	s := grpc.NewServer()
+	// pb.RegisterDatabaseServer(s, new(DatabaseService))
+	pb.RegisterArchiverServer(s, new(ArchiverService))
+
+	log.Print("Starting RPC server on port 8080...")
+	list, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("failed to setup tcp listener: %v", err)
+	}
+
+	err = s.Serve(list)
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+
+}
